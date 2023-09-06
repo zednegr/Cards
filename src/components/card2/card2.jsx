ac38651d130da8dcd1b28e0b15336e037025cf53
zednegr <negrzed@gmail.com>
2023-09-06 17:11:09 +0500

    card 2

--- src/components/card2/card2.jsx
+++ src/components/card2/card2.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
+import "./style.jsx";
+import {
+  CardTwoBottom,
+  CardTwo_Box,
+  CardTwo_Img,
+  CardTwo_Wrapper,
+  CardTwo_h4,
+  CardTwo_IconImg,
+  CardTwo_Div,
+  CardTwo_p,
+  CardTwo_Images
+} from "./style.jsx";
+
+import CardTwo from "../../assets/CardTwo.svg";
+import CardTwoAva from "../../assets/CardTwoAva.svg";
+import CardTwo_MassageIcon from "../../assets/CardTwo_MassageIcon.svg";
+import CardTwo_HeartIcon from "../../assets/CardTwo_HeartIcon.svg";
 
 export default function Card2() {
   return (
     <React.Fragment>
-      Card3
+      <CardTwo_Wrapper>
+        <CardTwo_Box>
+          <CardTwo_Images src={CardTwo} />
+
+          <CardTwoBottom>
+            <CardTwo_Div>
+              <CardTwo_Img src={CardTwoAva} />
+              <CardTwo_h4>Felipe Sacudon</CardTwo_h4>
+            </CardTwo_Div>
+
+            <CardTwo_Div>
+              <CardTwo_IconImg src={CardTwo_MassageIcon} />
+              <CardTwo_p>22</CardTwo_p>
+            </CardTwo_Div>
+
+            <CardTwo_Div>
+              <CardTwo_IconImg src={CardTwo_HeartIcon} />
+              <CardTwo_p>22</CardTwo_p>
+            </CardTwo_Div>
+          </CardTwoBottom>
+
+        </CardTwo_Box>
+      </CardTwo_Wrapper>
     </React.Fragment>
-  )
+  );
 }
---
+++ src/components/card2/style.jsx
@@ -0,0 +1,43 @@
+import { styled } from "styled-components";
+
+export const CardTwo_Images = styled.img`
+    border-radius:10px 10px 0 0;
+`
+
+export const CardTwo_Div = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const CardTwo_p = styled.p`
+  color: #000;
+  font-family: Roboto;
+  font-size: 12px;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+`;
+
+export const CardTwoBottom = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 10px;
+  gap: 20px;
+`;
+
+export const CardTwo_IconImg = styled.img`
+  cursor: pointer;
+`;
+
+export const CardTwo_Box = styled.div`
+  width: 280px;
+  height: auto;
+  background-color: #fff;
+  box-shadow: 4px 4px 8px 0px rgba(34, 60, 80, 0.2);
+  border-radius: 10px;
+`;
+
+export const CardTwo_Wrapper = styled.div``;
+export const CardTwo_h4 = styled.h4``;
+export const CardTwo_Img = styled.img``;
